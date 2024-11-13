213d69a7d9d0598a1b9e05f4bce968e5fa870cbd
Roberto Cunha <robertocunha@gmail.com>
2024-11-13 19:47:29 -0300

    Instala e configura clean-webpack-plugin
    npm install clean-webpack-plugin --save-dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,51 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');  // Adicione esta linha
 
 module.exports = {
   entry: {
-    index: './src/scripts/index.js',        // Entrada para a página inicial
-    createSong: './src/scripts/createSong.js' // Entrada para a página de criação de música
+    index: './src/scripts/index.js',
+    createSong: './src/scripts/createSong.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'  // Define o nome do bundle com base no nome da entrada
+    filename: '[name].bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          MiniCssExtractPlugin.loader,  // Extrai CSS em arquivos separados
-          'css-loader'                  // Carrega o CSS
+          MiniCssExtractPlugin.loader,
+          'css-loader'
         ]
       }
     ]
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),  // Define o diretório onde os arquivos estáticos ficam
+      directory: path.resolve(__dirname, 'dist'),
     },
-    open: true,           // Para abrir automaticamente no navegador
-    hot: true,            // Ativa o Hot Module Replacement
+    open: true,
+    hot: true,
   },
   plugins: [
+    new CleanWebpackPlugin(),  // Adicione esta linha
     new HtmlWebpackPlugin({
-      template: './src/index.html',  // O Webpack usará este arquivo como modelo
-      filename: 'index.html',        // O nome do arquivo de saída será index.html
-      inject: 'body',                // Injeta o script bundle.js no <body>
-      chunks: ['index']              // Este script será injetado apenas na página inicial
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      chunks: ['index']
     }),
     new HtmlWebpackPlugin({
-      template: './src/createSong.html', // O Webpack usará este arquivo como modelo
-      filename: 'createSong.html',       // O nome do arquivo de saída será createSong.html
-      inject: 'body',                   // Injeta o script bundle.js no <body>
-      chunks: ['createSong']            // Este script será injetado apenas na página de criação de música
+      template: './src/createSong.html',
+      filename: 'createSong.html',
+      inject: 'body',
+      chunks: ['createSong']
     }),
     new MiniCssExtractPlugin({
-      filename: 'main.css'           // O CSS gerado será nomeado como main.css
+      filename: 'main.css'
     })
   ],
   devtool: 'eval-source-map',
