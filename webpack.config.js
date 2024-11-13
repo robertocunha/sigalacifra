cfe63881437156e57776a9bacb92066b3ec37152
Roberto Cunha <robertocunha@gmail.com>
2024-11-13 19:41:14 -0300

    Configura Webpack para createSong

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/scripts/index.js',
+  entry: {
+    index: './src/scripts/index.js',        // Entrada para a página inicial
+    createSong: './src/scripts/createSong.js' // Entrada para a página de criação de música
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'  // Define o nome do bundle com base no nome da entrada
   },
   module: {
     rules: [
@@ -29,7 +32,15 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',  // O Webpack usará este arquivo como modelo
+      filename: 'index.html',        // O nome do arquivo de saída será index.html
       inject: 'body',                // Injeta o script bundle.js no <body>
+      chunks: ['index']              // Este script será injetado apenas na página inicial
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/createSong.html', // O Webpack usará este arquivo como modelo
+      filename: 'createSong.html',       // O nome do arquivo de saída será createSong.html
+      inject: 'body',                   // Injeta o script bundle.js no <body>
+      chunks: ['createSong']            // Este script será injetado apenas na página de criação de música
     }),
     new MiniCssExtractPlugin({
       filename: 'main.css'           // O CSS gerado será nomeado como main.css
