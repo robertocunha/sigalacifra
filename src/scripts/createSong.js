0cbd2f74b6d15087fc79fea9fb6b0cf0e60d4c1e
Roberto Cunha <robertocunha@gmail.com>
2024-11-14 18:14:56 -0300

    Implementa definicao de ultima posicao se indefinida

--- src/scripts/createSong.js
+++ src/scripts/createSong.js
@@ -4,7 +4,7 @@ import '../css/print.css';
 import '../css/style.css';
 
 import { app, db } from './firebaseConfig.js';
-import { doc, setDoc, collection } from "firebase/firestore";
+import { doc, setDoc, collection, query, orderBy, getDocs } from "firebase/firestore";
 
 // Referências aos elementos
 const form = document.getElementById("formId");
@@ -21,11 +21,33 @@ saveButton.addEventListener("click", async () => {
     const title = titleInput.value;
     const artist = artistInput.value;
     const tone = toneInput.value;
-    const position = Number(positionInput.value);
+    let position = Number(positionInput.value);
     const active = activeInput.checked;
     const lyrics = lyricsInput.value;
 
     console.log("formData: ", title, artist, tone, position, active, lyrics);
+
+    // Caso o campo posição esteja vazio, definimos a posição como a última
+    if (isNaN(position) || position === 0) {
+        try {
+            // Query para buscar o maior valor de posição existente
+            const q = query(collection(db, "musicas"), orderBy("position", "desc"));
+            const querySnapshot = await getDocs(q);
+            
+            // Se existirem músicas, pegamos o maior valor e somamos 10
+            if (!querySnapshot.empty) {
+                const lastDoc = querySnapshot.docs[0];
+                const lastPosition = lastDoc.data().position;
+                position = lastPosition + 10; // Espaçamento de 10
+            } else {
+                // Se não houver músicas, a posição será 10 (início da lista)
+                position = 10;
+            }
+        } catch (e) {
+            console.error("Erro ao buscar a última posição: ", e);
+            position = 10; // Definir posição padrão em caso de erro
+        }
+    }
 
     // Referência para o documento, deixando o Firestore gerar o ID automaticamente
     const docRef = doc(collection(db, "musicas"));
